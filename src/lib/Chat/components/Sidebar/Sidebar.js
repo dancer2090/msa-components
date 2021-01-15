bcf44495f333fba681f7a8a86ef77c12aed84e82
dancer2090 <ivaneichyk.ivan@gmail.com>
2021-01-15 13:26:12 +0200

    api amplify setup

--- src/lib/Chat/components/Sidebar/Sidebar.js
+++ src/lib/Chat/components/Sidebar/Sidebar.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Container } from './styles';
 import Button from '../common/Button';
-import store from '../../store';
 import Box from '../common/Box';
 import ChatListItem from '../common/ChatListItem';
+import { getChats } from '../../api';
 
 const Sidebar = () => {
 
   // const { state = {} } = useContext(store);
   // const { page, configuration } = state;
   const { portal = 'set the portal' } = useSelector(({ configuration }) => configuration);
+
+  const fetchInfo = async () => {
+    setTimeout(async () => {
+      const chats = await getChats();
+      console.log(chats);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
 
   return(
     <Box width="30%">
